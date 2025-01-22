bcecfbf8bce1ceec06d490d099801069eb8e0f9a
Aman11b <amansingh1100b@gmail.com>
2025-01-23 00:51:45 +0530

    added the header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <img src="./assets/globe.png" alt="globe_image"/>
+            <h1>My travel Journal</h1>
+        </header>
+    )
+}
