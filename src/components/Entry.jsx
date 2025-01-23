af1fbf5d5aa83d6f971ddb689c4940e2932914b5
Aman11b <amansingh1100b@gmail.com>
2025-01-24 02:22:42 +0530

    reduced the entry porps and used just entry

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -3,17 +3,17 @@ export default function Entry(props){
     return (
 
         <article className="journal-entry">
-            <img className="main-img" src={props.img.src} alt={props.img.alt} />
+            <img className="main-img" src={props.entry.img.src} alt={props.entry.img.alt} />
             <div className="info-container">
                 <img className="marker-logo" src="./assets/marker.png" alt="marker icon" />
-                <span className="country">{props.country}</span>
-                <a href={props.googleMapsLink}>
+                <span className="country">{props.entry.country}</span>
+                <a href={props.entry.googleMapsLink}>
                 view on Google maps
                 </a>
-                <h2 className="entry-title">{props.title}</h2>
-                <p className="trip-dates">{props.dates}</p>
+                <h2 className="entry-title">{props.entry.title}</h2>
+                <p className="trip-dates">{props.entry.dates}</p>
                 <p className="entry-text">
-                    {props.text}
+                    {props.entry.text}
                 </p>
             </div>
         </article>
